1dcfabf66e51fe710f8e02e5fef74eb469433e31
Gershey1 <german.adame@uabc.edu.mx>
2018-08-24 09:55:19 -0700

    Primera actualización

---
+++ src/paquete1/Test.java
@@ -0,0 +1,31 @@
+package paquete1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Operaciones.SUMA);
+		System.out.println(Operaciones.RESTA.name());
+		
+		double a = 2;
+		double b=4;
+		double res=Operaciones.SUMA.resultado(a, b);
+		
+		
+		
+		
+		/*
+		Operaciones ope= Operaciones.SUMA;
+		if(ope ==Operaciones.SUMA){
+			res=a+b;
+		}
+		else if(ope==Operaciones.RESTA){
+			res=a-b;
+		}*/
+		
+		System.out.println(res);
+		
+		
+	}
+
+}
